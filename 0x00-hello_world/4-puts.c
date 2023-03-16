8d43e9b39c08ed14f8d1a8722a1dbe006df01127
Irakozeelysee <elyseeirakoze@gmail.com>
2023-03-16 13:40:42 +0000

    c program with put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to print out and uses puts function
+ * Return:0 (it means that with 0 out program is successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
