be5530e8bc63bba1a5ba6bc109392eadea344c1a
Irakozeelysee <elyseeirakoze@gmail.com>
2023-03-17 18:23:31 +0000

    Digit separated by space and comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point.
+ * Description:  Single Digit numbers
+ * separated with comma and space
+ * add new line
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i > '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
