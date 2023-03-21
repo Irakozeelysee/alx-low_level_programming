6ff082a0983cddba29ca933071ff11571ca640ae
Irakozeelysee <elyseeirakoze@gmail.com>
2023-03-21 17:24:45 +0000

    print sings assigned to numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign -checks sign of a number.
+ *
+ * Return: 1 if positive, 0 if zero and -1 if neg.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
