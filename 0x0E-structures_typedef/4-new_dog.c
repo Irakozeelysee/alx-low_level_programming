21436f075038a6fe1498db381ca95153fc8b1a16
Irakozeelysee <elyseeirakoze@gmail.com>
2023-04-17 21:09:30 +0000

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include"dog.h"
 
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
  * *new_dog - Creates a new dog
  * @owner: name of owner
@@ -14,25 +17,68 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newdog = malloc(sizeof(dog_t));
+	dog_t *new_dog;
+	int name_l = 0, own_l = 0;
 
-	if (newdog == NULL)
+	if (name != NULL && owner != NULL)
 	{
+		name_l = strlen(name) + 1;
+		own_l = strlen(owner) + 1;
+		new_dog = malloc(sizeof(dog_t));
+
+		if (new_dog == NULL)
+			return (NULL);
+		new_dog->name = malloc(sizeof(char) * name_l);
+
+		if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
 	}
-	newdog->name = strdup(name);
-	if (newdog->name == NULL)
-	{
-		free(newdog);
-		return (NULL);
+		new_dog->owner = malloc(sizeof(char) * own_l);
+
+		if (new_dog->owner == NULL)
+		{
+			free(new_dog->name);
+			free(new_dog);
+			return (NULL);
+		}
+		new_dog->name = _strcpy(new_dog->name, name);
+		new_dog->owner = _strcpy(new_dog->owner, owner);
+		new_dog->age = age;
 	}
-	newdog->age = age;
-	newdog->owner = strdup(owner);
-	if (newdog->owner == NULL)
+	return (new_dog);
+}
+
+/**
+ * _strlen -Returns the length of string
+ * @s: String
+ * Return: String.
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
 	{
-		free(newdog->name);
-		free(newdog);
-		return (NULL);
+		c++;
 	}
-	return (newdog);
+	return (c);
+}
+/**
+ * _strcpy - Copies a string
+ * @dest: destination
+ * @src: source
+ * Return: The pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
 }
