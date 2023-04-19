b26c70e4f9279a8554f7a8f850b915bcb3c7c4eb
Irakozeelysee <elyseeirakoze@gmail.com>
2023-04-19 19:04:08 +0000

    a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,6 @@
 
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
 	int (*op_func)(int, int);
 
 	if (argc != 4)
@@ -19,41 +18,13 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
 
-	if (argv[2][1] != '\0')
+	if (!op_func)
 	{
 		printf("Error\n");
 		return (99);
 	}
-	switch (argv[2][0])
-	{
-		case '+':
-			op_func = get_op_func("+");
-			break;
-		case '-':
-			op_func = get_op_func("-");
-			break;
-		case '*':
-			op_func = get_op_func("*");
-			break;
-		case '/':
-			op_func = get_op_func("/");
-			break;
-		case '%':
-			op_func = get_op_func("%");
-			break;
-		default:
-			printf("Error\n");
-			return (99);
-	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		return (100);
-	}
-	result = op_func(num1, num2);
-	printf("%d\n", result);
+	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
