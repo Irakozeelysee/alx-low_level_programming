4e3ed81a7d1c262669bd43d40d7245e3dbea9eee
Irakozeelysee <elyseeirakoze@gmail.com>
2023-04-19 15:44:09 +0000

    a program that performs simple operations.

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,13 @@
 #ifndef _3_CALC_H_
 #define _3_CALC_H_
 
+#include <stdlib.h>
 /**
  * struct op - Struct op
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
 	char *op;
@@ -17,7 +19,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
 
 #endif
