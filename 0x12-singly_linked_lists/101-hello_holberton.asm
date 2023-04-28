a96a1ee710f114f30e429554ff7a6d5dae585ba2
Irakozeelysee <elyseeirakoze@gmail.com>
2023-04-28 22:03:40 +0000

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,11 @@
-global main
-extern printf
-
-msg db 'Hello, Holberton',0Ah,0
+global 	main
+	extern printf
 
 main:
-    push rbp
-    mov rbp, rsp
+	mov edi, format
+	xor eax, eax
+	call printf
 
-    mov edi, msg
-    xor eax, eax
-    call printf
-
-    mov edi, 0Ah
-    xor eax, eax
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax
-    ret
+	mov eax, 0Ah
+	ret
+format: db 'Hello, Holberton', 0Ah,0
