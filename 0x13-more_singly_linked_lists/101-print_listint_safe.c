e5d7d84b97719f405523c725f973ba8375d16b54
Irakozeelysee <elyseeirakoze@gmail.com>
2023-04-30 20:45:31 +0000

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+size_t _listint_len(const listint_t *head);
+/*_listint_len - countes number of nodes
+ * @head: pointer to the head of the listint_t
+ * Return: number of nodes
+ */
+
+size_t _listint_len(const listint_t *head)
+{
+	const listint_t *men, *women;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	men = head->next;
+	women = (head->next)->next;
+	while (women)
+	{
+		if (men == women)
+		{
+			men = head;
+			while (men != women)
+			{
+				nodes++;
+				men = men->next;
+				women = women->next;
+			}
+			men = men->next;
+			while (men != women)
+			{
+				nodes++;
+				men = men->next;
+			}
+			return (nodes);
+		}
+		men = men->next;
+		women = (women->next)->next;
+	}
+	return (0);
+}
 
 /**
  * print_listint_safe - Prints a listint_t linked list.
@@ -10,20 +49,25 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *temp;
-
-	while (head != NULL)
+	size_t nodes, count = 0;
+	
+	nodes = _listint_len(head);
+	if (nodes == 0)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		count++;
-		temp = head;
-		head = head->next;
-		if (temp <= head)
+		for (; head != NULL; nodes++)
 		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next;
 		}
 	}
-	return (count);
+	else
+	{
+		for (count = 0; count < nodes; count++)
+		{
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d]\n", (void *) head, head->n);
+	}
+	return (nodes);
 }
